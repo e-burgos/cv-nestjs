4c59abb14f1bd9e80ed398323c4ff0a03f91923f
Esteban Burgos <info@estebanburgos.com.ar>
2023-05-25 11:24:55 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BooksModule } from './books/books.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://eburgos:Esteban9414@cluster0.zzwjwkr.mongodb.net/',
+    ),
+    BooksModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
